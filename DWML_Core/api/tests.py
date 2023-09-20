6fc8ce22ef3cfbccc542fed39e935fb78a782f7a
Edwin Kassier <edwin@sweepsouth.com>
2023-09-20 18:14:25 +0200

    Update tests

--- DWML_Core/api/tests.py
+++ DWML_Core/api/tests.py
@@ -31,9 +31,9 @@ def test_base_route_malformed_no_symbol():
 @pytest.mark.django_db
 def test_base_route_malformed_no_investment():
     """Test base route with missing investment"""
-    response = client.get('/api/v1/process_request/?symbol=BTC')
+    response = client.get('/api/v1/process_request/?symbol=ETH')
 
-    assert response.status_code == 400
+    assert response.status_code == 200
 
 @pytest.mark.django_db
 def test_unknown_route():
